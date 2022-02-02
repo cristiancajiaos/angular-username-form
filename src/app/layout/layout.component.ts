2bf22aa60c8dd8379e51e6a126214166295cc177
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-02-02 12:09:49 -0300

    Implementado formulario responsivo

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  public myForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public isFieldInvalid(): boolean {
+    return this.myForm.controls['username'].invalid;
+  }
+
+  public requiredError(): boolean {
+    let control = this.myForm.controls['username'];
+    return control.touched && control.errors['required'];
+  }
+
+  public ngOnSubmit(): void {
+    console.log(this.myForm.value);
+  }
 }
